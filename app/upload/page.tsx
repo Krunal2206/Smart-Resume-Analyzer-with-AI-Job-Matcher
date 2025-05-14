e73f47cab53a175905acdc0b2f0454c6e7ee0f92
Krunal Thakar <kru.dipnil@gmail.com>
2025-05-14 16:44:03 -0400

    Add UploadPage and MongoDB connection functionality

---
+++ app/upload/page.tsx
@@ -0,0 +1,10 @@
+import UploadResumeForm from "../../components/UploadResumeForm";
+
+export default function UploadPage() {
+
+  return (
+    <section className="min-h-screen flex items-center justify-center px-4 py-20 bg-gray-950 text-white">
+      <UploadResumeForm />
+    </section>
+  );
+}
